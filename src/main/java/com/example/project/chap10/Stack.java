35d73f7ec60f38cfc149e64fa11df039ecb0e389
Zakariae Moutaouakil <moutaouakilzakariae.cpgea@gmail.com>
2024-03-08 21:51:42 +0100

    Added Stack data structure

---
+++ src/main/java/com/example/project/chap10/Stack.java
@@ -0,0 +1,50 @@
+package com.example.project.chap10;
+
+
+public class Stack<T> {
+    private final T[] stack;
+    private final int size;
+    private int top; // index of the top element
+
+    public Stack(int n) {
+        this.stack = (T[]) new Object[n];
+        this.size = n;
+        this.top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(T x) {
+        if (top == size - 1) {
+            throw new RuntimeException("Stack overflow");
+        } else {
+            top++;
+            stack[top] = x;
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        } else {
+            T element = stack[top];
+            top--;
+            return element;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i <= top; i++) {
+            sb.append(stack[i]).append(", ");
+        }
+        if (!isEmpty()) {
+            sb.setLength(sb.length() - 2);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/example/project/chap10/StackTest.java
@@ -0,0 +1,35 @@
+package com.example.project.chap10;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+public class StackTest {
+    @Test
+    void testStackOperations() {
+        Stack<Integer> stack1 = new Stack<>(5);
+        assertThat(stack1.toString()).isEqualTo("[]");
+        stack1.push(1);
+        assertThat(stack1.toString()).isEqualTo("[1]");
+        stack1.push(2);
+        stack1.push(100);
+        assertThat(stack1.toString()).isEqualTo("[1, 2, 100]");
+        assertThat(stack1.pop()).isEqualTo(100);
+        assertThat(stack1.toString()).isEqualTo("[1, 2]");
+        assertThat(stack1.pop()).isEqualTo(2);
+        assertThat(stack1.pop()).isEqualTo(1);
+        assertThat(stack1.isEmpty()).isTrue();
+        assertThrows(RuntimeException.class, stack1::pop);
+
+        // Check for overflow.
+        Stack<Integer> stack2 = new Stack<>(10);
+        for (int i = 0; i < 11; i++) {
+            if (i == 10) {
+                int finalI = i;
+                assertThrows(RuntimeException.class, () -> stack2.push(finalI));
+            } else {
+                stack2.push(i);
+            }
+        }
+        assertThat(stack2.toString()).isEqualTo("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]");
+    }
+}
